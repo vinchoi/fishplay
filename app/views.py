9c8b1aa3eabbed1b79d94b0057703326a65c2049
VIN-W <trojinspy@21cn.com>
2016-06-14 19:44:29 +0800

    1

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from app import app
+from flask import render_template
 
-@app.route('/')
+# @app.route('/')
 @app.route('/index')
 def index():
-    return 'helloworld'
+    return render_template('index.html')
+    # return 'hello'
+
+@app.route('/demo_page1')
+def demo_page1():
+    return render_template('demo_page1.html')
