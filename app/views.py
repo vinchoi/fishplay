811b3342d82a293695abe019de584390cda1feb1
vinchoi <1528657214@qq.com>
2016-06-18 19:32:48 +0800

    html

--- app/views.py
+++ app/views.py
@@ -10,3 +10,15 @@ def index():
 @app.route('/demo_page1')
 def demo_page1():
     return render_template('demo_page1.html')
+
+@app.route('/channel')
+def channel():
+    return render_template('channel.html')
+
+@app.route('/advertisers')
+def advertisers():
+    return render_template('advertisers.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
